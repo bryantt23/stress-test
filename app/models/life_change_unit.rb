dcf7b1a253ccf94fd358056d141e90eb3ad45259
bryantt23 <bryantt23@gmail.com>
2015-08-01 06:27:20 -0700

    displays message based on total score

--- app/models/life_change_unit.rb
+++ app/models/life_change_unit.rb
@@ -1,16 +1,28 @@
 class LifeChangeUnit < ActiveRecord::Base
 
-  def add(a=nil, b=nil, c=nil, d=nil)
+  def add(a=nil, b=0, c=0, d=0)
+
     if(a==nil)
+      message(0)
       0
-    elsif(b==nil)
-      a
-    elsif(c==nil)
-      a+b
     else
+      total=a+b+c+d
+      message(total)
       a+b+c+d
     end
   end
+
+  def message(total)
+    if(total>299)
+      puts "At risk of illness"
+    elsif (total>149)
+      puts "Risk of illness is moderate"
+    else
+      puts "Only have a slight risk of illness"
+    end
+  end
+
+
 
 
 end
--- test/models/life_change_unit_test.rb
+++ test/models/life_change_unit_test.rb
@@ -17,13 +17,26 @@ class LifeChangeUnitTest < ActiveSupport::TestCase
   test "with two inputs will add up the two values" do
     assert_equal 23, @life.add(5+18)
   end
-  
+
   test "with three inputs will add up the three values" do
     assert_equal 53, @life.add(20+30+3)
   end
 
   test "with four inputs will add up the four values" do
     assert_equal 250, @life.add(100,25,50,75)
+  end
+
+
+  test "Score of 300+: At risk of illness" do
+    assert_output(/At risk of illness/) { @life.add(305)}
+  end
+
+  test "Score of 150-299: Risk of illness is moderate (reduced by 30% from the above risk)." do
+    assert_output(/Risk of illness is moderate/){ @life.add(20+30+50+100)}
+  end
+
+  test "Score <150: Only have a slight risk of illness." do
+    assert_output(/Only have a slight risk of illness/){ @life.add(10,5,10)} 
   end
 
 
