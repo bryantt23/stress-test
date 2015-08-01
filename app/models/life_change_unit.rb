daf8746cb1a4a4827a2269478ed9037974cad61d
bryantt23 <bryantt23@gmail.com>
2015-08-01 06:00:14 -0700

    basic adding done

--- app/models/life_change_unit.rb
+++ app/models/life_change_unit.rb
@@ -1,7 +1,15 @@
 class LifeChangeUnit < ActiveRecord::Base
 
-  def add()
-    0
+  def add(a=nil, b=nil, c=nil, d=nil)
+    if(a==nil)
+      0
+    elsif(b==nil)
+      a
+    elsif(c==nil)
+      a+b
+    else
+      a+b+c+d
+    end
   end
 
 
--- test/models/life_change_unit_test.rb
+++ test/models/life_change_unit_test.rb
@@ -6,10 +6,26 @@ class LifeChangeUnitTest < ActiveSupport::TestCase
     @life = LifeChangeUnit.new
   end
 
+  test "has no input so will return 0" do
+    assert_equal 0, @life.add()
+  end
 
-    test "has no input so will return 0" do
-      assert_equal 0, @life.add()
-    end
+  test "with single input will return the value entered" do
+    assert_equal 5, @life.add(5)
+  end
+
+  test "with two inputs will add up the two values" do
+    assert_equal 23, @life.add(5+18)
+  end
+  
+  test "with three inputs will add up the three values" do
+    assert_equal 53, @life.add(20+30+3)
+  end
+
+  test "with four inputs will add up the four values" do
+    assert_equal 250, @life.add(100,25,50,75)
+  end
+
 
 
 
