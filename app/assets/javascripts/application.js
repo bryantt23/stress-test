f752174c8d4761869b070ac94deef030f3100741
bryantt23 <bryantt23@gmail.com>
2015-08-01 21:38:00 -0700

    add messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,27 +17,42 @@
 
 $('document').ready(function() {
 
-function displayVals() {
-      calcUsage();
-}
+  function displayVals() {
+    calcUsage();
+  }
 
+// variable for  checkboxes
+  var $cbs = $('input[name="ch1"]');
 
-var $cbs = $('input[name="ch1"]');
-
-function calcUsage() {
+  function calcUsage() {
     var total = 0;
-    // var total = $("#more").val();
+
+    // for each checkbox
     $cbs.each(function() {
-        if (this.checked)
-            total = parseInt(total) + parseInt(this.value);
+
+      // if this checkbox is checked
+      if (this.checked)
+
+      // parse the value and add it to the total
+      total = parseInt(total) + parseInt(this.value);
     });
-    $("#usertotal").text(total + ' QR');
-}
 
-    $("select").change(displayVals);
-    displayVals();
-//For  checkboxes
+//display message to user based on total
+//display in the div named usertotal
+    if(total>299){
+      $("#usertotal").text(total + ". At risk of illness.");
+    }
+    else if (total>149){
+      $("#usertotal").text(total +". Risk of illness is moderate.");
+    }
+    else{
+      $("#usertotal").text(total +". Only have a slight risk of illness.");
+    }
+  }
 
-$cbs.click(calcUsage);
+  $("select").change(displayVals);
+  displayVals();
+
+  $cbs.click(calcUsage);
 
 });
